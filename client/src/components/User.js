6a9dcfd8fa94112341d6b8bf1c7f80e3eddec873
KoChing Liang <yagamigin@gmail.com>
2017-06-28 00:09:13 +0800

    render user profile and stats

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -9,8 +9,58 @@ class User extends Component {
     super(props);
     const { username } = this.props.match.params;
     this.state = {
-      username: username
+      username: username,
+      user: {}
     };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${this.state.username}`)
+    .then(res => res.json())
+    .then(user => {
+      //console.log(user);
+      this.setState({ user });
+    })
+    .catch(error => console.log('ooops'))
+  }
+  
+  renderBasicProfile() {
+    const user = this.state.user;
+    return(
+      <div className="user-profile">
+        <a href={user.html_url} target="_blank" title={user.name || user.login}>
+          <img src={user.avatar_url}/>
+        </a>
+        <h2>
+          <a href={user.html_url} title={user.login} target="_blank">{user.name || user.login}</a>
+        </h2>
+        <h3>{user.location || 'I Live In My Mind'}</h3>
+      </div>
+    );
+  }
+
+  renderStat() {
+    console.log(this.state.user.html_url);
+    console.log(this.state.user.starred_url);
+    const user = this.state.user;
+    let followers = `${user.html_url}/followers`;
+    let repos = `${user.html_url}?/tab=repositories`;
+    let following = `${user.html_url}/following`;
+    return(
+      <div className="user-stats">
+        <ul>
+          <li>
+            <a href={followers} target="_blank" title="Number Of Followers"><i>{user.followers}</i><span>Followers</span></a>
+          </li>
+          <li>
+            <a href={repos} target="_blank" title="Number Of Repositoriy"><i>{user.public_repos}</i><span>Repositoriy</span></a>
+          </li>
+          <li>
+            <a href={following} target="_blank" title="Number Of Following"><i>{user.following}</i><span>Following</span></a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 
   render() {
@@ -19,6 +69,8 @@ class User extends Component {
         <div className="App-header">
           <h2>{this.state.username}</h2>
         </div>
+        {this.renderBasicProfile()}
+        {this.renderStat()}
       </div>
     );
   }
